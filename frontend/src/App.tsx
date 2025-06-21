033d6b38c31ef5f6e605e98017413d505dafd698
Rodrigo Vanegas <rvanegas@gmail.com>
2025-06-21 13:42:39 -0700

    diff breakpoint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -162,7 +162,7 @@ function App() {
 
   return (
     <div className="flex w-[100dvw] h-[100dvh]">
-      <div className="flex flex-col items-center w-[250px] bg-slate-200 dark:bg-zinc-800 md:block hidden">
+      <div className="flex flex-col items-center w-[250px] bg-slate-200 dark:bg-zinc-800 lg:block hidden">
         <button className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'>
           New Chat
         </button>
