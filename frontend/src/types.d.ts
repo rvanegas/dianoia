2fd16dfc5c196b998ef51f0c1280c86878982a76
Rodrigo Vanegas <rvanegas@gmail.com>
2025-06-30 21:43:19 -0700

    move more types into types.d.ts

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -17,3 +17,23 @@ export type ArgsType = {
   counter_argument: StepType[]
   assumptions: StepType[]
 }
+
+type ArgErrors = {
+  argument: string[]
+  counter_argument: string[]
+}
+
+type UserMode = 'thesis' | 'development' | 'inputProposition' | 'waiting'
+
+// thesis -> waiting -> thesis
+// thesis -> waiting -> development
+// development -> waiting -> development
+// development -> inputProposition -> waiting -> development
+
+type ConversationSnapshot = {
+  theses: ThesesType
+  args: ArgsType
+  argErrors: ArgErrors
+  lastPrompt: string
+  userMode: UserMode
+}
