6eb348c43133156c86f7e801aa255eebffc70430
Rodrigo Vanegas <rvanegas@gmail.com>
2025-07-07 10:58:04 -0700

    some more progress on fine grain support()

--- backend/services/conversation.py
+++ backend/services/conversation.py
@@ -5,12 +5,105 @@ import json
 
 from core.utils import logger
 from .system_prompt import (welcome_system_prompt, development_system_prompt,
-    justify_system_prompt)
+    justify_system_prompt, re_evaluate_system_prompt)
 from models.argument import (ArgumentPrompt, ArgumentResponse, ThesesPrompt,
-    JustifyPrompt, argument_response_format, theses_response_format,
-    justify_response_format, proofread_response, Step)
+    JustifyPrompt, Step, proofread_response)
 
 client = OpenAI(api_key=OPENAI_API_KEY)
+
+theses_response_format = {
+    "type": "json_schema",
+    "json_schema": {
+        "name": "response",
+        "strict": True,
+        "schema": {
+            "type": "object",
+            "properties": {
+                "thesis": {"type": "string"},
+                "counter_thesis": {"type": "string"},
+                "presupposition": {"type": "string"}
+            },
+            "required": ["thesis", "counter_thesis", "presupposition"],
+            "additionalProperties": False
+        }
+    }
+}
+
+argument_format = {
+    "type": "array",
+    "items": {
+        "type": "object",
+        "properties": {
+            "index": {"type": "string"},
+            "proposition": {"type": "string"},
+            "justifiers": {
+                "type": "array",
+                "items": {"type": "string"}
+            },
+            "truth": {"type": "number"},
+            "valid": {"type": "number"}
+        },
+        "required": ["index", "proposition", "justifiers", "truth", "valid"],
+        "additionalProperties": False
+    }
+}
+
+argument_response_format = {
+    "type": "json_schema",
+    "json_schema": {
+        "name": "response",
+        "strict": True,
+        "schema": {
+            "type": "object",
+            "properties": {
+                "assumptions": argument_format,
+                "argument": argument_format,
+                "counter_argument": argument_format
+            },
+            "required": ["argument", "counter_argument", "assumptions"],
+            "additionalProperties": False
+        }
+    }
+}
+
+justify_response_format = {
+    "type": "json_schema",
+    "json_schema": {
+        "name": "response",
+        "strict": True,
+        "schema": {
+            "type": "object",
+            "properties": {
+                "propositions": {
+                    "type": "array",
+                    "items": {"type": "string"}
+                }
+            },
+            "required": ["propositions"],
+            "additionalProperties": False
+        }
+    }
+}
+
+re_evaluate_response_format = {
+    "type": "json_schema",
+    "json_schema": {
+        "name": "response",
+        "strict": True,
+        "schema": {
+            "type": "object",
+            "properties": {
+                "truth": {
+                    "type": "array",
+                    "items": {"type": "number"}
+                },
+                "valid": {"type": "number"}
+            },
+            "required": ["truth", "valid"],
+            "additionalProperties": False
+        }
+    }
+}
 
 def develop_theses(theses_prompt):
     messages = [{
@@ -88,8 +181,38 @@ def justify_proposition(prompt: JustifyPrompt):
 
     new_propositions = json.loads(content)["propositions"]
     for p in new_propositions:
-        prompt.insert_proposition(p)
+        new_arg = prompt.insert_proposition(p)
+
+    evaluations = re_evaluate(new_arg)
+
+    logger.debug(f"evaluations{evaluations}")
 
     new_args = prompt.json()
+    logger.debug(f"new_arg{new_arg}")
+    logger.debug(f"new_args{new_args}")
 
     return new_args, None
+
+def re_evaluate(steps: list[Step]):
+    props = [s.proposition for s in steps]
+
+    messages = [{
+        "role": "system",
+        "content": re_evaluate_system_prompt
+    },
+    {
+        "role": "user",
+        "content": json.dumps(props)
+    }]
+
+    response = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        response_format=re_evaluate_response_format,
+    )
+    content = response.choices[0].message.content
+
+    evaluations = json.loads(content)
+    logger.debug(f"evaluations{evaluations}")
+
+    return evaluations
--- backend/services/system_prompt.py
+++ backend/services/system_prompt.py
@@ -137,3 +137,31 @@ property. The JSON returned specifies these one or two propositions as separate
 strings.
 
 """
+
+re_evaluate_system_prompt = """
+
+You are a logical assistant in an argument clinic. Your task is to help the
+user develop and articulate arguments in syllogistic form.
+
+You will receive a list of propositions. In response, you will return an array of
+numbers from 0.0 to 1.0, rounded to nearest 0.05, each corresponding to a given
+proposition in the same order.
+
+In each case, the number should be 1.0 if the proposition is certainly true, 0.0
+if it is certainly not true, and otherwise represent the degree to which it is
+likely to be true.
+
+Set this array to the property "truth".
+
+Additionally, concerning the last proposition in the list, you will return one
+number from 0.0 to 1.0, rounded to the nearest 0.05, corresponding to the validity
+of the inference from the other propositions to the last one. That is, assuming
+the other propositions are certainly true, then this number represents the likelihood
+that the last proposition is true. In case of deduction, set to 1.0. In case of
+contradiction, set to 0.0. Otherwise, determine the implicit premise that would
+make the inference a deduction and set the value to the likelihood that this
+proposition is true.
+
+Set this number to the property "valid".
+
+"""
