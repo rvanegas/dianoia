cc05d5be6c2654364ff1a2ae132b02853582c375
Rodrigo Vanegas <rvanegas@gmail.com>
2025-07-11 18:01:31 -0700

    refactor Argument classes

--- backend/api/v1/chat.py
+++ backend/api/v1/chat.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from core.utils import logger
 from models.argument import (ThesesPrompt, Arguments,
-    ArgumentsWithStepPrompt, ArgumentsWithPrompt)
+    ArgumentsWithStep, ArgumentsWithProposition)
 
 router = APIRouter()
 
@@ -11,22 +11,22 @@ async def theses(prompt: ThesesPrompt):
     theses = prompt.develop()
     return {"reply": theses}
 
-@router.post("/ai-justify")
-async def justify(prompt: ArgumentsWithStepPrompt):
-    args = prompt.justify()
-    return {"reply": args}
-
-@router.post("/remove")
-async def remove(prompt: ArgumentsWithStepPrompt):
-    args = prompt.remove()
-    return {"reply": args}
-
 @router.post("/evaluate")
 async def evaluate(prompt: Arguments):
     args = prompt.evaluate()
     return {"reply": args}
 
+@router.post("/remove")
+async def remove(prompt: ArgumentsWithStep):
+    args = prompt.remove()
+    return {"reply": args}
+
+@router.post("/ai-justify")
+async def justify(prompt: ArgumentsWithStep):
+    args = prompt.justify()
+    return {"reply": args}
+
 @router.post("/user-justify")
-async def user_justify(prompt: ArgumentsWithPrompt):
+async def user_justify(prompt: ArgumentsWithProposition):
     args = prompt.user_justify()
     return {"reply": args}
