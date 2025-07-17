a81c3fed9b20ab240b3fbc1f392bf110a07eef18
Rodrigo Vanegas <rvanegas@gmail.com>
2025-07-17 14:19:58 -0700

    remove argErrors

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -18,17 +18,11 @@ export type ArgsType = {
   assumptions: StepType[]
 }
 
-type ArgErrors = {
-  argument: string[]
-  counter_argument: string[]
-}
-
 type ArgMode = 'thesis' | 'development'
 
 type ConversationSnapshot = {
   theses: ThesesType
   args: ArgsType
-  argErrors: ArgErrors
   lastPrompt: string
   argMode: ArgMode
   explanation: string | undefined
