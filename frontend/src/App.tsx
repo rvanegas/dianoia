5c88b37adbf49ee08c802b9ba3039ac51ecba7f2
Rodrigo Vanegas <rvanegas@gmail.com>
2025-10-09 13:32:51 -0700

    use immer, introduce state btw components, Conversation state not yet updating App state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,65 @@
 import './App.css'
-// import React, {createContext, useReducer, useContext} from 'react'
+import {useImmer} from 'use-immer'
 
-import type { ConversationType } from './types'
+import type {ConversationType} from './types'
 
 import Conversation from './Conversation'
 
-const initialState: ConversationType = {
-  index: 1,
+const initialState0: ConversationType = {
+  id: 0,
+  name: '',
+  snapshots: []
+}
+
+const initialState1: ConversationType = {
+  id: 1,
   name: '',
   snapshots: []
 }
 
 function App() {
-  const newConversation = (index: string, proposition: string) => {
-    console.log(`newConversation ${index} ${proposition} ${initialState}`)
+  const [conversations, setConversations] = useImmer<ConversationType[]>([initialState0, initialState1])
+  const [currConvIndex, setCurrConvIndex] = useImmer<number>(0)
+
+  const setConversation = (newConversation: ConversationType) => {
+    setConversations(c => {c[currConvIndex] = newConversation})
   }
+
+  const createConversation = (proposition: string) => {
+    console.log(`newConversation ${proposition}`)
+  }
+
+  const selectConversation = (index: number) => {
+    setCurrConvIndex(index)
+  }
+
+  console.log('x', currConvIndex, conversations)
 
   return (
     <div className="flex w-[100dvw] h-[100dvh]">
       <div className="flex flex-col items-center w-[250px] bg-slate-200 dark:bg-zinc-800 lg:block hidden">
-        <button className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'>
-          New Chat
+        <button
+          className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'
+          onClick={() => createConversation('')}>
+          New
+        </button>
+        <button
+          className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'
+          onClick={() => selectConversation(0)}>
+          Select0
+        </button>
+        <button
+          className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'
+          onClick={() => selectConversation(1)}>
+          Select1
         </button>
       </div>
       <div className="flex flex-1 flex-col h-[100%] w-[100%] bg-white items-center" >
-        <Conversation newConversation={newConversation}/>
+        <Conversation
+          conversationIndex={currConvIndex}
+          conversation={conversations[currConvIndex]}
+          setConversation={setConversation}
+          createConversation={createConversation}/>
       </div>
     </div>
   )
