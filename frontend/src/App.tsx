f0b6f75a09a56ed6e97c7e69cad265e800f9f83a
Rodrigo Vanegas <rvanegas@gmail.com>
2025-10-10 17:52:44 -0700

    distinguish id/index some more

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,14 +6,14 @@ import type {ConversationType} from './types'
 import Conversation from './Conversation'
 
 const initialState: ConversationType = {
-  id: 0,
+  id: 1,
   name: '',
   snapshots: []
 }
 
 function App() {
   const [conversations, setConversations] = useImmer<ConversationType[]>(
-    [{...initialState, id: 0}, {...initialState, id: 1}])
+    [{...initialState, id: 1}, {...initialState, id: 2}])
   const [currConvIndex, setCurrConvIndex] = useImmer<number>(0)
 
   const setConversation = (newConversation: ConversationType) => {
@@ -41,18 +41,17 @@ function App() {
         <button
           className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'
           onClick={() => selectConversation(0)}>
-          Select0
+          Select1
         </button>
         <button
           className='w-[80%] px-2 py-4 m-4 text-white bg-indigo-500 border-none rounded-2xl'
           onClick={() => selectConversation(1)}>
-          Select1
+          Select2
         </button>
       </div>
       <div className="flex flex-1 flex-col h-[100%] w-[100%] bg-white items-center">
         <Conversation
           key={currConvIndex}
-          conversationIndex={currConvIndex}
           conversation={conversations[currConvIndex]}
           setConversation={setConversation}
           createConversation={createConversation}/>
