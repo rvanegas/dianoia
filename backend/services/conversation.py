39c8d98af603d33775f2ae04d90ae1401da9251d
Rodrigo Vanegas <rvanegas@gmail.com>
2025-10-16 21:57:16 -0700

    refactor conversation service

--- backend/services/conversation.py
+++ backend/services/conversation.py
@@ -1,29 +1,58 @@
+from enum import Enum
+from dataclasses import dataclass
 from openai import OpenAI
 from config import OPENAI_API_KEY
 from core.utils import logger
 
-from .system_prompt import (welcome_system_prompt, development_system_prompt,
-    justify_system_prompt, evaluate_system_prompt)
+from .system_prompt import (
+    theses_system_prompt,
+    develop_system_prompt,
+    justify_system_prompt,
+    evaluate_system_prompt)
 
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-theses_response_format = {
-    "type": "json_schema",
-    "json_schema": {
-        "name": "response",
-        "strict": True,
-        "schema": {
-            "type": "object",
-            "properties": {
-                "thesis": {"type": "string"},
-                "counter_thesis": {"type": "string"},
-                "presupposition": {"type": "string"}
-            },
-            "required": ["thesis", "counter_thesis", "presupposition"],
-            "additionalProperties": False
+@dataclass
+class Gpt:
+    system_prompt: dict
+    response_format: str
+
+    def call(self, prompt: str):
+        messages = [{
+            "role": "system",
+            "content": self.system_prompt
+        },
+        {
+            "role": "user",
+            "content": prompt
+        }]
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            response_format=self.response_format,
+        )
+        return response.choices[0].message.content
+
+gpt_theses = Gpt(
+    system_prompt=theses_system_prompt,
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "response",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "thesis": {"type": "string"},
+                    "counter_thesis": {"type": "string"},
+                    "presupposition": {"type": "string"}
+                },
+                "required": ["thesis", "counter_thesis", "presupposition"],
+                "additionalProperties": False
+            }
         }
     }
-}
+)
 
 argument_format = {
     "type": "array",
@@ -44,123 +73,69 @@ argument_format = {
     }
 }
 
-argument_response_format = {
-    "type": "json_schema",
-    "json_schema": {
-        "name": "response",
-        "strict": True,
-        "schema": {
-            "type": "object",
-            "properties": {
-                "assumptions": argument_format,
-                "argument": argument_format,
-                "counter_argument": argument_format
-            },
-            "required": ["argument", "counter_argument", "assumptions"],
-            "additionalProperties": False
-        }
-    }
-}
-
-justify_response_format = {
-    "type": "json_schema",
-    "json_schema": {
-        "name": "response",
-        "strict": True,
-        "schema": {
-            "type": "object",
-            "properties": {
-                "propositions": {
-                    "type": "array",
-                    "items": {"type": "string"}
-                }
-            },
-            "required": ["propositions"],
-            "additionalProperties": False
-        }
-    }
-}
-
-evaluate_response_format = {
-    "type": "json_schema",
-    "json_schema": {
-        "name": "response",
-        "strict": True,
-        "schema": {
-            "type": "object",
-            "properties": {
-                "truth": {
-                    "type": "array",
-                    "items": {"type": "number"}
+gpt_develop = Gpt(
+    system_prompt=develop_system_prompt,
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "response",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "assumptions": argument_format,
+                    "argument": argument_format,
+                    "counter_argument": argument_format
                 },
-                "valid": {"type": "number"}
-            },
-            "required": ["truth", "valid"],
-            "additionalProperties": False
+                "required": ["argument", "counter_argument", "assumptions"],
+                "additionalProperties": False
+            }
         }
     }
-}
+)
 
-def gpt_welcome(prompt: str):
-    messages = [{
-        "role": "system",
-        "content": welcome_system_prompt
-    },
-    {
-        "role": "user",
-        "content": prompt
-    }]
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        response_format=theses_response_format,
-    )
-    return response.choices[0].message.content
+gpt_justify = Gpt(
+    system_prompt=justify_system_prompt,
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "response",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "propositions": {
+                        "type": "array",
+                        "items": {"type": "string"}
+                    }
+                },
+                "required": ["propositions"],
+                "additionalProperties": False
+            }
+        }
+    }
+)
 
-def gpt_develop(prompt: str):
-    messages = [{
-        "role": "system",
-        "content": development_system_prompt
-    },
-    {
-        "role": "user",
-        "content": prompt
-    }]
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        response_format=argument_response_format,
-    )
-    return response.choices[0].message.content
+gpt_evaluate = Gpt(
+    system_prompt=evaluate_system_prompt,
+    response_format={
+        "type": "json_schema",
+        "json_schema": {
+            "name": "response",
+            "strict": True,
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "truth": {
+                        "type": "array",
+                        "items": {"type": "number"}
+                    },
+                    "valid": {"type": "number"}
+                },
+                "required": ["truth", "valid"],
+                "additionalProperties": False
+            }
+        }
+    }
+)
 
-def gpt_justify(prompt: str):
-    messages = [{
-        "role": "system",
-        "content": justify_system_prompt
-    },
-    {
-        "role": "user",
-        "content": prompt
-    }]
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        response_format=justify_response_format,
-    )
-    return response.choices[0].message.content
-
-def gpt_evaluate(props: str):
-    messages = [{
-        "role": "system",
-        "content": evaluate_system_prompt
-    },
-    {
-        "role": "user",
-        "content": props
-    }]
-    response = client.chat.completions.create(
-        model="gpt-4o",
-        messages=messages,
-        response_format=evaluate_response_format,
-    )
-    return response.choices[0].message.content
