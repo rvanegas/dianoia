c5a16d33c9b907a14cdc517f0af5e0ecaa630898
Rodrigo Vanegas <rvanegas@gmail.com>
2025-10-22 02:53:42 -0700

    new strategy. one permanent assistant, and many transient vector_stores, each with one file.

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,10 @@
 """env variables"""
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
+OPENAI_MODEL = os.getenv("OPENAI_MODEL")
 DATABASE_URL = os.getenv("DATABASE_URL")
-VECTOR_STORE_ID = os.getenv("VECTOR_STORE_ID")
