4e4b684a871cd2687a2fcdfd7f2b08ca492eb5a7
Rodrigo Vanegas <rvanegas@gmail.com>
2025-11-02 17:24:46 -0700

    rename chat to argument

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from api.v1.chat import router as chat_router
+from api.v1.argument import router as argument_router
 
 app = FastAPI()
 
@@ -13,4 +13,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(chat_router, prefix="/api/v1")
+app.include_router(argument_router, prefix="/api/v1")
